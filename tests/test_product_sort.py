00be53c1a17b02401197662e671d67c2081c5b3f
coralgruper <coralgruper@gmail.com>
2024-10-29 18:14:20 +0200

    Update test_product_sort.py

--- tests/test_product_sort.py
+++ tests/test_product_sort.py
@@ -1,11 +1,9 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from src.src_login_page import LoginPage
 from src.src_inventory_page import InventoryPage
-from selenium.webdriver.support.ui import Select
 
 
 # Fixture to set up and tear down the Selenium WebDriver
@@ -21,7 +19,7 @@ def setup_driver():
     login.set_username("standard_user")
     login.set_password("secret_sauce")
     login.login_button()
-    yield driver  # Yield driver instead of login for broader use
+    yield driver
     driver.quit()
 
 
@@ -36,7 +34,8 @@ def test_default_sort(setup_driver):
     sorted_items_names = sorted(items_names)  # Sort items list for comparison
 
     try:
-        # Verify default sort type
+        # Verify default sort type of the dropdown is "Name (A to Z)" and check that the items are indeed sorted by
+        # ascending alphabetical order
         default_inventory_sort_text = inventory_page.get_sort_type_text()
         expected_inventory_sort_text = "Name (A to Z)"
         assert default_inventory_sort_text == expected_inventory_sort_text, "Default order is not correct."
@@ -53,24 +52,56 @@ def test_sort_name_Z_A(setup_driver):
     # Fetch inventory items and check if they are sorted by descending names
     inventory_page.select_sort_type_text("Name (Z to A)")
     items_names = inventory_page.get_inventory_items_names()
-    sorted_items_names_desc = sorted(items_names, reverse=True)  # Sort items list in descending order for comparison
+    reverse_sorted_items_names = sorted(items_names, reverse=True)  # Sort items list in descending order for comparison
 
     try:
-        # Verify descending name sort (Z to A)
+        # Verify that when selecting sort by "Name (Z to A)" from the dropdown list it is indeed selected, check that
+        # the order of the items is now sorted by descending alphabetical order
         selected_inventory_sort_text = inventory_page.get_sort_type_text()
         expected_inventory_sort_text = "Name (Z to A)"
         assert selected_inventory_sort_text == expected_inventory_sort_text, "Sort type text does not match."
-        assert items_names == sorted_items_names_desc, "Items are not correctly ordered by Name (Z to A)."
+        assert items_names == reverse_sorted_items_names, "Items are not correctly ordered by Name (Z to A)."
     except NoSuchElementException:
         pytest.fail("Could not verify the sort option or item order.")
 
 
-"""
-
 # Sort by price from low to high
-def test_sort_price_low_high(self):
+def test_sort_price_low_high(setup_driver):
+    inventory_page = InventoryPage()
+    inventory_page.driver = setup_driver  # Use the setup driver for the inventory page
+
+    # Fetch inventory items and check if they are sorted by ascending prices
+    inventory_page.select_sort_type_text("Price (low to high)")
+    items_prices = inventory_page.get_inventory_items_prices()
+    sorted_items_prices = sorted(items_prices)  # Sort items list in ascending order for comparison
+
+    try:
+        # Verify that when selecting sort by "Price (low to high)" from the dropdown list it is indeed selected,
+        # check that the order of the items is now sorted by by ascending numerical order
+        selected_inventory_sort_text = inventory_page.get_sort_type_text()
+        expected_inventory_sort_text = "Price (low to high)"
+        assert selected_inventory_sort_text == expected_inventory_sort_text, "Sort type text does not match."
+        assert items_prices == sorted_items_prices, "Items are not correctly ordered by Price (low to high)."
+    except NoSuchElementException:
+        pytest.fail("Could not verify the sort option or item order.")
+
 
 # Sort by Price high to low
-def test_sort_price_high_low(self):
+def test_sort_price_high_low(setup_driver):
+    inventory_page = InventoryPage()
+    inventory_page.driver = setup_driver  # Use the setup driver for the inventory page
 
-"""
+    # Fetch inventory items and check if they are sorted by ascending prices
+    inventory_page.select_sort_type_text("Price (high to low)")
+    items_prices = inventory_page.get_inventory_items_prices()
+    reverse_sorted_items_prices = sorted(items_prices, reverse=True)  # Sort items list in descending order for comparison
+
+    try:
+        # Verify that when selecting sort by "Price (high to low)" from the dropdown list it is indeed selected,
+        # check that the order of the items is now sorted by by descending numerical order
+        selected_inventory_sort_text = inventory_page.get_sort_type_text()
+        expected_inventory_sort_text = "Price (high to low)"
+        assert selected_inventory_sort_text == expected_inventory_sort_text, "Sort type text does not match."
+        assert items_prices == reverse_sorted_items_prices, "Items are not correctly ordered by Price (high to low)."
+    except NoSuchElementException:
+        pytest.fail("Could not verify the sort option or item order.")
